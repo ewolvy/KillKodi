b199781712d7888e86b8933a532d615ee7c6fc09
ewolvy <juanjosoriano@gmail.com>
2016-11-24 13:49:11 +0100

    Menú y opciones completado

--- app/src/main/java/com/mooo/ewolvy/killkodi/MainActivity.java
+++ app/src/main/java/com/mooo/ewolvy/killkodi/MainActivity.java
@@ -1,8 +1,15 @@
 package com.mooo.ewolvy.killkodi;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +19,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                startActivity(new Intent(MainActivity.this,
+                        SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void KillKodi(View v){
         //TODO: Code to connect to the Linux machine running Kodi
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.confirm_title))
+                .setMessage(getString(R.string.confirm_message))
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Toast.makeText(MainActivity.this, "Yaay", Toast.LENGTH_SHORT).show();
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
